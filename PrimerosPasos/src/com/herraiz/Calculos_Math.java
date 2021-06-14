e7222a61fe1dc8b5d4e49fdea431c60fe16f1d48
Rober Herraiz <herraiz92@gmail.com>
2021-06-14 15:40:06 +0100

    Added Math.sqrt()

---
+++ PrimerosPasos/src/com/herraiz/Calculos_Math.java
@@ -0,0 +1,13 @@
+package com.herraiz;
+
+public class Calculos_Math {
+
+    public static void main(String[] args) {
+
+        double raiz = Math.sqrt(9);    // Importante que el tipo de la variable pueda almacenar el resultado de Math.sqrt(), que es double
+
+        System.out.println(raiz);
+
+
+    }
+}

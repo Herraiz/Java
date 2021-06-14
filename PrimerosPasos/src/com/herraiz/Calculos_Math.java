1ff7e1562cd2b49337c4007975431ea93c9698e4
Rober Herraiz <herraiz92@gmail.com>
2021-06-14 16:00:18 +0100

    Last methods for Math class

--- PrimerosPasos/src/com/herraiz/Calculos_Math.java
+++ PrimerosPasos/src/com/herraiz/Calculos_Math.java
@@ -4,9 +4,37 @@ public class Calculos_Math {
 
     public static void main(String[] args) {
 
-        double raiz = Math.sqrt(9);    // Importante que el tipo de la variable pueda almacenar el resultado de Math.sqrt(), que es double
+        // Math.sqrt()
+
+        double raiz = Math.sqrt(9);
 
         System.out.println(raiz);
+
+        // Math.round()
+
+        float num1 = 5.85F;
+
+        int resultado  = Math.round(num1);
+
+        System.out.println(resultado);
+
+
+        // Refundición, forzamos la conversión pese a la pérdida de información
+
+        double num2 = 5.85;
+
+        int resultado2  = (int)Math.round(num2);
+
+        System.out.println(resultado2);
+
+        // Potencias
+
+        double base = 5;
+        double exponente = 3;
+
+        double potencia = Math.pow(base, exponente);
+
+        System.out.println("El resultado de la potencia de base " + base + " y exponente " + exponente + " es " + potencia);
 
 
     }

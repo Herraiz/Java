1fd88120ec0d95c59940ccf91cd5bcc94e1f2fca
Rober Herraiz <herraiz92@gmail.com>
2021-06-14 12:51:04 +0100

    Now working fine with IntelliJ

---
+++ PrimerosPasosIntelliJ/src/com/herraiz/Main.java
@@ -0,0 +1,11 @@
+package com.herraiz;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        byte edad = 35;
+        System.out.println("La edad es " + edad);
+    }
+}

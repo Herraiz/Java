c8c47180147fe46b1a5d710dbafaf6836b5664c7
Rober Herraiz <herraiz92@gmail.com>
2021-06-14 16:53:59 +0100

    Added substring method

--- PrimerosPasos/src/com/herraiz/Cadenas.java
+++ PrimerosPasos/src/com/herraiz/Cadenas.java
@@ -15,17 +15,30 @@ public class Cadenas {
         int ultima_letra = nombre.length(); // Cuenta el 0
 
         System.out.println("La última letra es " + nombre.charAt(ultima_letra - 1)); // Le quitamos 1 para "descontar" el 0
-        
+
 
         // Vídeo 12
 
+        System.out.println();
+
         String frase = "Hoy es un estupendo día para aprender a programar en Java";
+
+        System.out.println("La frase es: " + frase);
 
         String subfrase = frase.substring(29, 49);
 
-        System.out.println(subfrase);
+        System.out.println("La subfrase es: " + subfrase);
 
 
+        System.out.println();
+
+        String alumno1, alumno2;
+
+        alumno1 = "David";
+        alumno2 = "david";
+
+        System.out.println("Las cadenas son iguales? -> " + alumno1.equals(alumno2));
+        System.out.println("Las cadenas, ignorando mayúsculas, son iguales? -> " + alumno1.equalsIgnoreCase(alumno2));
 
     }
 }

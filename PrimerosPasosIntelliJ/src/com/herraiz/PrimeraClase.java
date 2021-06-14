c852b01974909d525d325ba313cd0effa630320e
Rober Herraiz <herraiz92@gmail.com>
2021-06-14 15:06:16 +0100

    First class renamed

---
+++ PrimerosPasosIntelliJ/src/com/herraiz/PrimeraClase.java
@@ -0,0 +1,18 @@
+package com.herraiz;
+
+public class PrimerosPasos {
+
+    /* Comentario multi línea,
+    útil para explicar cosas largas
+     */
+
+    public static void main(String[] args) {
+
+        byte edad = 35;  // Variable
+
+        final double a_pulgadas = 2.54; // Constante
+
+        System.out.println("La edad es " + edad);
+
+    }
+}

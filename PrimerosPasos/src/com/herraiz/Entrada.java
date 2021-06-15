54366982454d63129361d71d35f803f2f23a5d50
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-06-15 15:31:15 +0100

    New program, asking user some info

---
+++ PrimerosPasos/src/com/herraiz/Entrada.java
@@ -0,0 +1,27 @@
+package com.herraiz;
+
+import java.util.Scanner;
+
+public class Entrada {
+
+    public static void main(String[] args) {
+
+        // Invocamos al constructor
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Introduce tu nombre, por favor");
+
+        // Pedimos al usuario unos datos
+
+        String nombre_usuario = entrada.nextLine();
+
+        System.out.println("Introduce tu edad, por favor");
+
+        int edad_usuario = entrada.nextInt();
+
+        System.out.println("Hola " + nombre_usuario + ". El año que viene tendrás " + (edad_usuario + 1) + " años."); // Importante el paréntesis
+
+    }
+
+}

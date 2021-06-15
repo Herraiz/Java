a8633bc964b7cf11ef7bf9e594a345f01c17038a
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-06-15 15:08:20 +0100

    Import packages to our code

---
+++ PrimerosPasos/src/com/herraiz/Importar_Paquetes.java
@@ -0,0 +1,24 @@
+package com.herraiz;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Importar_Paquetes {
+
+    public static void main(String[] args) {
+
+        String nombre;
+
+        Scanner miobjeto;  // Esto da error si no se importa el package
+
+        /*
+        import java.util.Scanner;          <--  Importar solo la clase Scanner
+        import java.util.*;                <--  Importar el paquete entero
+         */
+
+        Locale miobjeto2;
+
+        // Con Code -> Optimize imports te pone los imports necesarios de forma automática si lo configuras bien
+
+    }
+}

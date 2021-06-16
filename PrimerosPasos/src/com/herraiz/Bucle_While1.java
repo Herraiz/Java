1e4849aca357c0855d8ef584dea363e46145ef6a
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-06-16 16:47:17 +0100

    feat: Added excercise with while loop and some JOptionPane dialogs

---
+++ PrimerosPasos/src/com/herraiz/Bucle_While1.java
@@ -0,0 +1,30 @@
+package com.herraiz;
+
+import javax.swing.*;
+
+public class Bucle_While1 {
+
+    public static void main(String[] args) {
+
+        String hardcoded_pass = "Roberto";
+
+        String pass = "";
+
+        while (!hardcoded_pass.equals(pass)) {
+
+            pass = JOptionPane.showInputDialog(null,
+                    "Por favor, introduce la contraseña",
+                    "Acceso a la aplicación",
+                    JOptionPane.INFORMATION_MESSAGE);
+
+            if (!hardcoded_pass.equals(pass)) {
+                JOptionPane.showMessageDialog(null,
+                        "Contraseña incorrecta");
+            }
+
+        }
+
+        JOptionPane.showMessageDialog(null,
+                "Contraseña correcta. Acceso permitido");
+    }
+}

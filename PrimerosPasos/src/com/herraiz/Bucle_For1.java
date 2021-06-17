5fb96da742000861bc72f035d1ab3812ad58f760
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-06-17 18:46:17 +0100

    feat: Just a simple for loop

---
+++ PrimerosPasos/src/com/herraiz/Bucle_For1.java
@@ -0,0 +1,12 @@
+package com.herraiz;
+
+public class Bucle_For1 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i += 2) {
+            System.out.println("Hola mundo " + i);
+        }
+
+    }
+}

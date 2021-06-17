df4f63d259d35b11b68494f4cd83061c62765c8a
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-06-17 19:43:35 +0100

    feat: arrays explanation

---
+++ PrimerosPasos/src/com/herraiz/Array_1.java
@@ -0,0 +1,30 @@
+package com.herraiz;
+
+public class Array_1 {
+
+    public static void main(String[] args) {
+
+//        int[] mi_matriz = new int[5];
+//
+//        mi_matriz[0] = 5;
+//        mi_matriz[1] = 65;
+//        mi_matriz[2] = 32;
+//        mi_matriz[3] = 12;
+//        mi_matriz[4] = 8;
+//
+//        System.out.println(mi_matriz[3] + "\n");
+
+        int[] mi_matriz = {5, 65, 32, 12, 8};
+
+        /* Enhanced for loop */
+        for (int i : mi_matriz) {
+            System.out.println(i);
+        }
+
+        for (:
+             ) {
+            
+        }
+
+    }
+}

74174ab5c2586fe459e24f0aa887300766990f80
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-06-21 13:21:44 +0100

    feat: learning about OOP, classes and objects created the new class Coche and initialite it on Uso_Coche

---
+++ PrimerosPasos/src/poo/Coche.java
@@ -0,0 +1,23 @@
+package poo;
+
+public class Coche {
+
+    int ruedas;
+    int largo;
+    int ancho;
+    int motor;
+    int peso;
+
+    /* Método constructor */
+
+    public Coche() {
+
+        ruedas = 4;
+        largo = 2000;
+        ancho = 300;
+        motor = 1600;
+        peso = 500;
+
+    }
+
+}
---
+++ PrimerosPasos/src/poo/Uso_Coche.java
@@ -0,0 +1,14 @@
+package poo;
+
+public class Uso_Coche {
+
+    public static void main(String[] args) {
+
+        /* INSTANCIAR LA CLASE - RENAULT ES UN EJEMPLO DE LA CLASE COCHE */
+        Coche Renault = new Coche();
+
+        System.out.println("Este coche tiene " + Renault.ruedas + " ruedas");
+
+    }
+
+}

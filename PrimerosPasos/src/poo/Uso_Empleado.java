5b2f9e7a12e187b62b5ba27aa7e6124db72584a5
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-06-22 18:04:02 +0100

    feat: Created the base for the new class Empleado

---
+++ PrimerosPasos/src/poo/Uso_Empleado.java
@@ -0,0 +1,25 @@
+package poo;
+
+import java.util.Date;
+
+public class Uso_Empleado {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
+
+class Empleado {
+
+    private String name;
+    private double salary;
+    private Date contractRegistration;
+
+    /* Constructor */
+    public Empleado(String name, double salary, int year, int month, int day) {
+
+    }
+
+}

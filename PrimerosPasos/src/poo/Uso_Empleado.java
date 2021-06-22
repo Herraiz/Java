fda0c3c2345e743154de9927e6bf44819b5414d0
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-06-22 18:25:34 +0100

    feat: Added all the methods to the class Empleado
    needs testing

--- PrimerosPasos/src/poo/Uso_Empleado.java
+++ PrimerosPasos/src/poo/Uso_Empleado.java
@@ -1,6 +1,7 @@
 package poo;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Uso_Empleado {
 
@@ -20,6 +21,25 @@ class Empleado {
     /* Constructor */
     public Empleado(String name, double salary, int year, int month, int day) {
 
+        this.name = name;
+        this.salary = salary;
+        GregorianCalendar calendar = new GregorianCalendar(year, month - 1, day);
+        this.contractRegistration = calendar.getTime();
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public Date getContractRegistration() {
+        return contractRegistration;
+    }
+
+    public void raiseSalary(double percent) {
+        salary += salary * percent/100;
+    }
 }

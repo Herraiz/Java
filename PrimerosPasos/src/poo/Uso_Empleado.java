a7e86f0f81434c467658759b9cea1042380b5df4
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-06-22 18:53:20 +0100

    feat: finished the Empleado excercise

--- PrimerosPasos/src/poo/Uso_Empleado.java
+++ PrimerosPasos/src/poo/Uso_Empleado.java
@@ -7,16 +7,63 @@ public class Uso_Empleado {
 
     public static void main(String[] args) {
 
+//        Empleado empleado1 = new Empleado(
+//            "Paco Gómez", 85000, 1990, 12, 17);
+//
+//        Empleado empleado2 = new Empleado(
+//            "Ana López", 95000, 1995, 6, 2);
+//
+//        Empleado empleado3 = new Empleado(
+//            "María Martín", 105000, 2002, 3, 15);
+//
+//
+//        empleado1.raiseSalary(5);
+//        empleado2.raiseSalary(5);
+//        empleado3.raiseSalary(5);
+//
+//        /* Imprimimos los datos */
+//
+//        System.out.println("\nNombre: " + empleado1.getName()
+//                + "\nSueldo: " + empleado1.getSalary() + "\nFecha de alta: "
+//                + empleado1.getContractRegistration());
+//
+//        System.out.println("\nNombre: " + empleado2.getName()
+//                + "\nSueldo: " + empleado2.getSalary() + "\nFecha de alta: "
+//                + empleado2.getContractRegistration());
+//
+//        System.out.println("\nNombre: " + empleado3.getName()
+//                + "\nSueldo: " + empleado3.getSalary() + "\nFecha de alta: "
+//                + empleado3.getContractRegistration());
+
+
+        Empleado[] misEmpleados = new Empleado[3];
+
+        misEmpleados[0] = new Empleado(
+                "Paco Gómez", 85000, 1990, 12, 17);
+
+        misEmpleados[1] = new Empleado(
+                "Ana López", 95000, 1995, 6, 2);
+
+        misEmpleados[2] = new Empleado(
+                "María Martín", 105000, 2002, 3, 15);
+
+        for (Empleado empleado : misEmpleados) {
+            empleado.raiseSalary(5);
+        }
+
+        for (Empleado empleado : misEmpleados) {
+            System.out.println("Nombre: " + empleado.getName()
+                    + " - Sueldo: " + empleado.getSalary() + " - Fecha de alta: "
+                    + empleado.getContractRegistration());
+        }
 
     }
-
 }
-
 class Empleado {
 
-    private String name;
+    private final String name;
     private double salary;
-    private Date contractRegistration;
+    private final Date contractRegistration;
 
     /* Constructor */
     public Empleado(String name, double salary, int year, int month, int day) {
@@ -40,6 +87,6 @@ class Empleado {
     }
 
     public void raiseSalary(double percent) {
-        salary += salary * percent/100;
+        salary += salary * percent / 100;
     }
 }

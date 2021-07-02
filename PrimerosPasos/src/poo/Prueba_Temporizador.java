af1cb84fb7687e26a87622baabff6dd892e3cf20
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-07-02 12:48:38 +0100

    perf(GetTime): added sound to actionPerformed

--- PrimerosPasos/src/poo/Prueba_Temporizador.java
+++ PrimerosPasos/src/poo/Prueba_Temporizador.java
@@ -1,6 +1,7 @@
 package poo;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Date;
@@ -27,11 +28,14 @@ public class Prueba_Temporizador {
 
 class GetTime implements ActionListener {
 
+
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
         Date now = new Date();
         System.out.println("Te pongo la hora cada 5 seg: " + now);
+        Toolkit.getDefaultToolkit().beep(); // sonido nativo
 
     }
 }

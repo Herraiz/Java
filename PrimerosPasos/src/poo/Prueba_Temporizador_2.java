a7b1a0a91e9ed918dd7fb1b4fd485938e3539d9f
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-07-02 13:26:58 +0100

    perf: now we use a local inner class

--- PrimerosPasos/src/poo/Prueba_Temporizador_2.java
+++ PrimerosPasos/src/poo/Prueba_Temporizador_2.java
@@ -10,9 +10,9 @@ public class Prueba_Temporizador_2 {
 
     public static void main(String[] args) {
 
-        Clock myClock = new Clock(3000, true);
+        Clock myClock = new Clock();
 
-        myClock.start();
+        myClock.start(3000, true);
 
         JOptionPane.showMessageDialog(null, "Pulsa en Aceptar para terminar");
 
@@ -23,36 +23,29 @@ public class Prueba_Temporizador_2 {
 
 class Clock {
 
-    private final int interval;
-    private final boolean sound;
+    public void start(int interval, final boolean sound) {
 
-    public Clock(int interval, boolean sound) {
-        this.interval = interval;
-        this.sound = sound;
+        /* Clase interna local - La usamos porque solo instanciamos GetTime2 una vez */
+        class GetTime2 implements ActionListener {
 
-    }
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
-    public void start() {
+                Date now = new Date();
+                System.out.println("Te pongo la hora cada " + interval/1000 + " seg: " + now);
+
+                if (sound) {
+                    Toolkit.getDefaultToolkit().beep();
+                }
+
+            }
+        }
+
         ActionListener listener = new GetTime2();
 
         Timer myTimer = new Timer(interval, listener);
 
         myTimer.start();
-    }
-
-    private class GetTime2 implements ActionListener {
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-
-            Date now = new Date();
-            System.out.println("Te pongo la hora cada " + interval/1000 + " seg: " + now);
-
-            if (sound) {
-                Toolkit.getDefaultToolkit().beep();
-            }
-
-        }
     }
 
 }

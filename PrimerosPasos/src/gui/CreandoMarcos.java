680b6d78a787f563383ffc45544fef9d331b60d2
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-07-02 16:45:53 +0100

    feat: created our first frame based on JFrame

---
+++ PrimerosPasos/src/gui/CreandoMarcos.java
@@ -0,0 +1,25 @@
+package gui;
+
+import javax.swing.*;
+
+public class CreandoMarcos {
+
+    public static void main(String[] args) {
+
+        MiMarco marco1 = new MiMarco();
+        marco1.setVisible(true);
+        marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+}
+
+class MiMarco extends JFrame {
+
+    public MiMarco(){
+
+        setSize(500, 300);
+
+    }
+
+}

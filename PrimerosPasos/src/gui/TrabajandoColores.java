00b2844a4e1aef71769339662174533f161b9c4e
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-07-03 19:23:55 +0100

    feat: frame background color and some color draws

---
+++ PrimerosPasos/src/gui/TrabajandoColores.java
@@ -0,0 +1,61 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+
+public class TrabajandoColores {
+
+    public static void main(String[] args) {
+
+        MarcoConColor mainFrame = new MarcoConColor();
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setVisible(true);
+
+    }
+}
+
+class MarcoConColor extends JFrame {
+
+    public MarcoConColor() {
+
+        setTitle("Prueba con colores");
+        setSize(400, 400);
+
+        PanelConColor myPanel = new PanelConColor();
+        add(myPanel);
+        myPanel.setBackground(SystemColor.window);
+
+    }
+
+}
+
+class PanelConColor extends JPanel {
+
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        Color myGreen = new Color(0, 205, 94);
+        Color myGray = new Color(154, 154, 154);
+
+        Rectangle2D rectangle = new Rectangle2D.Double(100, 100, 200, 150);
+
+        g2.setPaint(Color.BLUE);
+        g2.draw(rectangle);
+
+        g2.setPaint(myGray);
+        g2.fill(rectangle);  //  Con fill en vez de draw, rellenamos con el color indicado
+
+        Ellipse2D ellipse = new Ellipse2D.Double();
+        ellipse.setFrame(rectangle);
+
+        g2.setPaint(myGreen);
+        g2.fill(ellipse);
+
+    }
+}

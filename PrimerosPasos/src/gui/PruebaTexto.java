343bb12600e6db52d2b5570557a6d2753383f141
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-07-13 13:18:59 +0100

    feat: added logic to the email validator using DameTexto class and ActionListener

--- PrimerosPasos/src/gui/PruebaTexto.java
+++ PrimerosPasos/src/gui/PruebaTexto.java
@@ -1,6 +1,7 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -27,27 +28,58 @@ class MarcoTexto extends JFrame {
 }
 
 class PanelTexto extends JPanel {
+
     private final JTextField field1;
+    private final JLabel result;
 
     public PanelTexto() {
+
+        setLayout(new BorderLayout());
+        JPanel subPanel = new JPanel();
+        subPanel.setLayout(new FlowLayout());
+
+        result = new JLabel();
+        result.setHorizontalAlignment(JLabel.CENTER);
+        add(result, BorderLayout.CENTER);
+
         JLabel text1 = new JLabel("Email:");
-        add(text1);
+        subPanel.add(text1);
 
         field1 = new JTextField(20);
-        add(field1);
+        subPanel.add(field1);
+
 
         JButton myButton = new JButton("Comprobar");
 
         DameTexto myEvent = new DameTexto();
         myButton.addActionListener(myEvent);
-        add(myButton);
+        subPanel.add(myButton);
+
+        add(subPanel, BorderLayout.NORTH);
     }
 
     private class DameTexto implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            System.out.println(field1.getText());
+
+            int correct = 0;
+            String email = field1.getText().trim();
+
+            for (int i = 0; i < email.length(); i++) {
+                if (email.charAt(i) == '@') {
+                    correct++;
+                }
+            }
+
+            if (correct != 1) {
+                System.out.println("Email incorrecto");
+                result.setText("Incorrecto");
+            } else {
+                System.out.println("Email correcto");
+                result.setText("Correcto");
+            }
+
         }
     }
 

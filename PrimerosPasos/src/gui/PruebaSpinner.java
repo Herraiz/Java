7a104f143f8fdec3bc74ea502493853690acddba
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-07-20 12:51:45 +0100

    perf: used a lambda function to override some methods of the SpinnerNumberModel

--- PrimerosPasos/src/gui/PruebaSpinner.java
+++ PrimerosPasos/src/gui/PruebaSpinner.java
@@ -27,13 +27,45 @@ class PanelSpinner extends JPanel {
 
     public PanelSpinner() {
 
-//        String list[] = {"Enero", "Febrero", "Marzo", "Abril"};
+//        String list[] = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
 
-        JSpinner control = new JSpinner(new SpinnerNumberModel(5, 0, 10, 1));
+        /* Usaremos una clase interna anónima para sobrescribir los métodos sin escribir mucho */
+
+        JSpinner control = new JSpinner(new SpinnerNumberModel(5, 0, 10, 1) {
+
+            @Override
+            public Object getNextValue() {
+                return super.getPreviousValue();
+            }
+
+            @Override
+            public Object getPreviousValue() {
+                return super.getNextValue();
+            }
+
+        });
 
         control.setPreferredSize(new Dimension(100, 20));
 
         add(control);
     }
 
+    /* Forma clásica, heredando y sustituyendo
+    private class MiModeloJSpinner extends SpinnerNumberModel {
+
+        public MiModeloJSpinner() {
+            super(5, 0, 10, 1);
+        }
+
+        @Override
+        public Object getNextValue() {
+            return super.getPreviousValue();
+        }
+
+        @Override
+        public Object getPreviousValue() {
+            return super.getNextValue();
+        }
+    }
+     */
 }

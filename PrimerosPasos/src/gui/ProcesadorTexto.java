76c4650a99c86275ff882f866e1bf71c0b09852b
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-08-03 13:02:58 +0100

    feat: now the style menu uses JCheckBoxMenuItem and size one uses JRadioButtonMenuItem

--- PrimerosPasos/src/gui/ProcesadorTexto.java
+++ PrimerosPasos/src/gui/ProcesadorTexto.java
@@ -48,17 +48,39 @@ class PanelProcesador extends JPanel {
 
         /* Style submenu **/
         style = new JMenu("Estilo");
-        menuConfig("Negrita", "Estilo", "", Font.BOLD, 1, "./src/gui/bold.png");
-        menuConfig("Cursiva", "Estilo", "", Font.ITALIC, 1, "./src/gui/italic.png");
-
+//        menuConfig("Negrita", "Estilo", "", Font.BOLD, 1, "./src/gui/bold.png");
+//        menuConfig("Cursiva", "Estilo", "", Font.ITALIC, 1, "./src/gui/italic.png");
+        JCheckBoxMenuItem bold = new JCheckBoxMenuItem("Negrita", new ImageIcon("./src/gui/bold.png"));
+        JCheckBoxMenuItem italic = new JCheckBoxMenuItem("Cursiva", new ImageIcon("./src/gui/italic.png"));
+        style.add(bold);
+        style.add(italic);
+        bold.addActionListener(new StyledEditorKit.BoldAction());
+        italic.addActionListener(new StyledEditorKit.ItalicAction());
 
         /* Size submenu **/
         size = new JMenu("Tamaño");
-        menuConfig("12", "Tamaño", "", 9, 12, "");
-        menuConfig("16", "Tamaño", "", 9, 16, "");
-        menuConfig("20", "Tamaño", "", 9, 20, "");
-        menuConfig("24", "Tamaño", "", 9, 24, "");
+//        menuConfig("12", "Tamaño", "", 9, 12, "");
+//        menuConfig("16", "Tamaño", "", 9, 16, "");
+//        menuConfig("20", "Tamaño", "", 9, 20, "");
+//        menuConfig("24", "Tamaño", "", 9, 24, "");
 
+        ButtonGroup fontSize = new ButtonGroup();
+        JRadioButtonMenuItem size12 = new JRadioButtonMenuItem("12");
+        JRadioButtonMenuItem size16 = new JRadioButtonMenuItem("16");
+        JRadioButtonMenuItem size20 = new JRadioButtonMenuItem("20");
+        JRadioButtonMenuItem size24 = new JRadioButtonMenuItem("24");
+        fontSize.add(size12);
+        fontSize.add(size16);
+        fontSize.add(size20);
+        fontSize.add(size24);
+        size.add(size12);
+        size.add(size16);
+        size.add(size20);
+        size.add(size24);
+        size12.addActionListener(new StyledEditorKit.FontSizeAction("Change size", 12));
+        size16.addActionListener(new StyledEditorKit.FontSizeAction("Change size", 16));
+        size20.addActionListener(new StyledEditorKit.FontSizeAction("Change size", 20));
+        size24.addActionListener(new StyledEditorKit.FontSizeAction("Change size", 24));
 
 
         /* Text Panel */
@@ -92,6 +114,7 @@ class PanelProcesador extends JPanel {
                     menuItem.addActionListener(new StyledEditorKit.FontFamilyAction("Change size", "Courier"));
                 }
             }
+            /*
             case "Estilo" -> {
                 this.style.add(menuItem);
                 if (style == Font.BOLD) {
@@ -99,7 +122,7 @@ class PanelProcesador extends JPanel {
                 } else if ((style == Font.ITALIC)) {
                     menuItem.addActionListener(new StyledEditorKit.ItalicAction());
                 }
-            }
+            } */
             case "Tamaño" -> {
                 this.size.add(menuItem);
                 menuItem.addActionListener(new StyledEditorKit.FontSizeAction("Change size", size));

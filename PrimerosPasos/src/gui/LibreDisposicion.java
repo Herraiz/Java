ab7a5ea0a10836d96af1893773cf891d94330a20
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-08-10 13:02:45 +0100

    feat: created our first custom layout using LayoutManager

--- PrimerosPasos/src/gui/LibreDisposicion.java
+++ PrimerosPasos/src/gui/LibreDisposicion.java
@@ -1,6 +1,7 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class LibreDisposicion {
 
@@ -26,17 +27,18 @@ class PanelLibre extends JPanel {
 
     public PanelLibre() {
 
-        setLayout(null); // disposición libre
+        setLayout(new EnColumnas());
         JLabel name = new JLabel("Nombre: ");
         JLabel surname = new JLabel("Apellidos: ");
         JTextField nameField = new JTextField();
         JTextField surnameField = new JTextField();
 
+        /* Deprecated -> Disposición libre setLayout(null)
         name.setBounds(20, 20, 80, 10);
         surname.setBounds(20, 50, 80, 10);
 
         nameField.setBounds(85, 15, 100, 20);
-        surnameField.setBounds(85, 45, 100, 20);
+        surnameField.setBounds(85, 45, 100, 20); */
 
 
         add(name);
@@ -44,6 +46,59 @@ class PanelLibre extends JPanel {
         add(surname);
         add(surnameField);
 
+        add(new JLabel("Edad: "));
+        add(new JTextField());
+
     }
 
+}
+
+class EnColumnas implements LayoutManager {
+
+    private int x = 20;
+    private int y = 20;
+
+    @Override
+    public void addLayoutComponent(String name, Component comp) {
+
+    }
+
+    @Override
+    public void removeLayoutComponent(Component comp) {
+
+    }
+
+    @Override
+    public Dimension preferredLayoutSize(Container parent) {
+        return null;
+    }
+
+    @Override
+    public Dimension minimumLayoutSize(Container parent) {
+        return null;
+    }
+
+    @Override
+    public void layoutContainer(Container parent) {
+        int counter = 0;
+        int n = parent.getComponentCount();
+
+        for (int i = 0; i < n; i++) {
+
+            counter++;
+
+            Component comp = parent.getComponent(i);
+            comp.setBounds(x, y, 100, 20);
+
+            /* Location magic - pairs */
+
+            x += 80;
+
+            if (counter % 2 == 0) {
+                x = 20;
+                y += 25;
+            }
+        }
+
+    }
 }

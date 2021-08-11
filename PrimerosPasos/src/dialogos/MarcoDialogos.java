9f26759c3be45a560bcf7acc94ba9f6172aa97e0
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-08-11 12:24:41 +0100

    feat: finished the gui

--- PrimerosPasos/src/dialogos/MarcoDialogos.java
+++ PrimerosPasos/src/dialogos/MarcoDialogos.java
@@ -5,33 +5,53 @@ import java.awt.*;
 
 public class MarcoDialogos extends JFrame {
 
-    private PanelBotones typePanel;
+    private PanelBotones typePanel, messagetypePanel, confirmPanel,
+            optionPanel, messagePanel, inputPanel;
 
     public MarcoDialogos() {
 
         setTitle("Prueba de Diálogos");
         setBounds(500, 300, 600, 450);
 
-        JPanel panelGrid = new JPanel();
-        panelGrid.setLayout(new GridLayout(2, 3));
-        add(panelGrid);
 
-        String[] primero = {"Mensaje", "Confirmar", "Opcion", "Entrada"};
-        typePanel = new PanelBotones("Tipo", primero);
+        JPanel panelGrid = new JPanel();
+        setLayout(new BorderLayout());
+        panelGrid.setLayout(new GridLayout(2, 3));
+        add(panelGrid, BorderLayout.CENTER);
+
+        /* Radio Buttons */
+
+        String[] typeOptions = {"Mensaje", "Confirmar", "Opción", "Entrada"};
+        typePanel = new PanelBotones("Tipo", typeOptions);
         panelGrid.add(typePanel);
 
-        
-        panelGrid.add(new PanelBotones("Confirmar", new String[]{"ERROR.MESSAGE", "INFORMATION_MESSAGE",
-                "WARNING_MESSAGE", "QUESTION_MESSAGE",
-                "PLAIN_MESSAGE"}));
+        String[] confirmOptions = {"DEFAULT_OPTION", "YES_NO_OPTION", "YES_NO_CANCEL_OPTION", "OK_CANCEL_OPCION"};
+        confirmPanel = new PanelBotones("Confirmar", confirmOptions);
+        panelGrid.add(confirmPanel);
 
-        panelGrid.add(new PanelBotones("Tipo de Mensaje", new String[]{"Mensaje", "Confirmar", "Opcion", "Entrada"}));
 
-        panelGrid.add(new PanelBotones("Opción", new String[]{"Mensaje", "Confirmar", "Opcion", "Entrada"}));
+        String[] messageTypeOptions = {"ERROR_MESSAGE", "INFORMATION_MESSAGE",
+                "WARNING_MESSAGE", "QUESTION_MESSAGE", "PLAIN_MESSAGE"};
+        messagetypePanel = new PanelBotones("Confirmar", messageTypeOptions);
+        panelGrid.add(messagetypePanel);
 
-        panelGrid.add(new PanelBotones("Mensaje", new String[]{"Mensaje", "Confirmar", "Opcion", "Entrada"}));
+        String[] optionOptions = {"String[]", "Icon[]", "Object[]"};
+        optionPanel = new PanelBotones("Opción", optionOptions);
+        panelGrid.add(optionPanel);
 
-        panelGrid.add(new PanelBotones("Entrada", new String[]{"Mensaje", "Confirmar", "Opcion", "Entrada"}));
+        String[] messageOptions = {"Cadena", "Icono", "Componente", "Otros", "Object[]"};
+        messagePanel = new PanelBotones("Mensaje", messageOptions);
+        panelGrid.add(messagePanel);
+
+        String[] inputOptions = {"Campo de texto", "Combo"};
+        inputPanel = new PanelBotones("Entrada", inputOptions);
+        panelGrid.add(inputPanel);
+
+        /* Show Panel */
+        JPanel showPanel = new JPanel();
+        JButton showButton = new JButton("Mostrar");
+        showPanel.add(showButton);
+        add(showPanel, BorderLayout.SOUTH);
 
 
     }

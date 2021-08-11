59077dab49edc2ccc3ab4a5d3f247235b0507949
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-08-11 13:15:04 +0100

    feat: added a fist selected item and a getSelection method

--- PrimerosPasos/src/dialogos/PanelBotones.java
+++ PrimerosPasos/src/dialogos/PanelBotones.java
@@ -4,21 +4,34 @@ import javax.swing.*;
 
 public class PanelBotones extends JPanel {
 
+    public ButtonGroup buttonGroup;
+
     public PanelBotones(String title, String[] options) {
 
         setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), title));
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup = new ButtonGroup();
+
+        int counter = 0;
 
         for (String option : options) {
-
             JRadioButton radioButton = new JRadioButton(option);
+            radioButton.setActionCommand(option); // necessary for getSelection
             add(radioButton);
             buttonGroup.add(radioButton);
 
-        }
+            if (counter == 0) {
+                radioButton.setSelected(true);
+            }
 
+            counter++;
+
+        }
+    }
+
+    public String getSelection() {
+        return buttonGroup.getSelection().getActionCommand();
     }
 
 

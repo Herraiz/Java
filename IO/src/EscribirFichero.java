adeee8e8dfefa0e07c05cd5aee0a9256b68a4d04
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-08-24 13:27:04 +0100

    feat: writting in a text file

---
+++ IO/src/EscribirFichero.java
@@ -0,0 +1,33 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class EscribirFichero {
+
+    public static void main(String[] args) {
+
+        Writing writing = new Writing();
+        writing.write();
+
+    }
+
+}
+
+class Writing {
+
+    public void write(){
+        String text = "Esto es una prueba de escritura";
+
+        try {
+            FileWriter writer = new FileWriter("res/ejemplo_escribir.txt");
+                writer.write(text);
+
+                /* Es necesario hacer el close para que pueda escribir, por tema de buffers */
+                writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}

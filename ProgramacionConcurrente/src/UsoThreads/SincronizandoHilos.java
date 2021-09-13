016637adbb606ec393434dc4245f9fcb6cdf2e98
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-09-13 11:56:48 +0100

    perf: added the .join() to the class HilosVarios2

--- ProgramacionConcurrente/src/UsoThreads/SincronizandoHilos.java
+++ ProgramacionConcurrente/src/UsoThreads/SincronizandoHilos.java
@@ -5,22 +5,14 @@ public class SincronizandoHilos {
     public static void main(String[] args) {
 
       HilosVarios thread1 = new HilosVarios();
-      HilosVarios thread2 = new HilosVarios();
+      /* Nuestro HiloVarios2 estará supeditado a que finalice el hilo1 para ejecutarse */
+      HilosVarios2 thread2 = new HilosVarios2(thread1);
+
+      thread2.start();
       thread1.start();
 
-        try {
-            thread1.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
-        thread2.start();
 
-        try {
-            thread2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
         System.out.println("Terminadas las tareas");
     }
@@ -31,6 +23,35 @@ class HilosVarios extends Thread {
 
     @Override
     public void run() {
+        for (int i = 0; i < 15; i++) {
+            System.out.println("Ejecutando hilo: " + this.getName());
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
+
+class HilosVarios2 extends Thread {
+
+    private Thread t;
+
+    public HilosVarios2(Thread t){
+        this.t = t;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         for (int i = 0; i < 15; i++) {
             System.out.println("Ejecutando hilo: " + this.getName());
             try {

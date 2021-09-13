228e920f876246760193e8443268279b98a251e6
Rober Herraiz <18282688+RoberHerraiz@users.noreply.github.com>
2021-09-13 12:47:41 +0100

    feat: base program finished, out of sync threads

--- ProgramacionConcurrente/src/UsoThreads/BancoSinSincronizar.java
+++ ProgramacionConcurrente/src/UsoThreads/BancoSinSincronizar.java
@@ -1,12 +1,20 @@
 package UsoThreads;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class BancoSinSincronizar {
 
     public static void main(String[] args) {
 
+        Banco banco = new Banco();
 
+        for (int i = 0; i<100; i++) {
+            EjecucionTransferencias r = new EjecucionTransferencias(banco, i, 2000);
+            Thread t = new Thread(r);
+            t.start();
+        }
     }
-
 }
 
 class Banco {
@@ -16,11 +24,7 @@ class Banco {
     public Banco() {
         cuentas = new double[100];
 
-        for (int i = 0; i<cuentas.length; i++) {
-
-            cuentas[i] = 2000;
-
-        }
+        Arrays.fill(cuentas, 2000);
 
     }
 
@@ -30,13 +34,13 @@ class Banco {
             return;
         }
 
-        System.out.println("Iniciando transferencia en " + Thread.currentThread());
+        System.out.println("Iniciando transferencia en " + Thread.currentThread().getName());
 
         cuentas[origen] -= cantidad;
-        System.out.printf("%10.2f de %d para %d", cantidad, origen, destino);
+        System.out.printf("Transferencia de %10.2f € de %d para %d ", cantidad, origen, destino);
         cuentas[destino] += cantidad;
 
-        System.out.printf("Saldo total: %10.2f%n", getSaldoTotal());
+        System.out.printf("\nSaldo total: %10.2f%n", getSaldoTotal());
 
     }
 
@@ -50,4 +54,39 @@ class Banco {
         return sumaCuentas;
     }
 
+}
+
+class EjecucionTransferencias implements Runnable {
+
+    private Banco banco;
+    private int origen;
+    private double cantidadMax;
+
+    public EjecucionTransferencias(Banco b, int origen, double max){
+
+        banco = b;
+        this.origen = origen;
+        this.cantidadMax = max;
+
+
+    }
+
+    @Override
+    public void run() {
+
+        while (true) {
+            try {
+                int destino = (int) (100 * Math.random());
+                double cantidad = cantidadMax * Math.random();
+
+                banco.transferencia(origen, destino, cantidad);
+
+                    Thread.sleep((long) (Math.random() * 10));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
 }

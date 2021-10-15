5ee7a8e041fd344a67e10c6867472d6cf29f03ff
Rober Herraiz <herraiz92@gmail.com>
2021-10-15 16:04:35 +0100

    perf: now we can delete elements on the colection using iterators

--- Colecciones/Default/src/CuentasUsuarios.java
+++ Colecciones/Default/src/CuentasUsuarios.java
@@ -1,4 +1,5 @@
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class CuentasUsuarios {
 
@@ -20,12 +21,39 @@ public class CuentasUsuarios {
         Cliente cl5 = new Cliente("Julio Iglesias", "00004", 350000);
         clientesBanco.add(cl5);
 
+
+        /* Podemos eliminar fácilmente objetos dentro de una colección con un iterador */
+
+        Iterator<Cliente> it = clientesBanco.iterator();
+
+        while(it.hasNext()) {
+            String clientName = it.next().getName();
+            if (clientName.equals("Julio Iglesias")) {
+                it.remove();
+            }
+        }
+
+        /* Recorremos con un for - each; es útil para mostrar información */
+
         for (Cliente cliente : clientesBanco) {
+
             System.out.println(cliente.getName() + " "
                     + cliente.getAccountName() + " "
                     + cliente.getBalance());
 
         }
+
+        /* Recorremos con un iterador
+
+        Iterator<Cliente> it = clientesBanco.iterator();
+        while(it.hasNext()) {
+
+            String clientName = it.next().getName();
+
+
+            System.out.println(clientName);
+
+        } */
 
     }
 }

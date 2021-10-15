b1155d7125e778d9d478942ce4aae58b3c3f6d05
Rober Herraiz <herraiz92@gmail.com>
2021-10-15 15:26:51 +0100

    feat: example finished

--- Colecciones/Default/src/Libro.java
+++ Colecciones/Default/src/Libro.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Libro {
 
     String title;
@@ -10,7 +12,36 @@ public class Libro {
         this.ISBN = ISBN;
     }
 
+    /* Compararemos por ISBN - Lo podemos hacer manualmente o pedirle a Intellij que lo genere
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Libro) {
 
+            Libro other = (Libro)obj;
+
+            if (this.ISBN == other.ISBN) {
+                return true;
+            }
+
+        }
+
+        return false;
+    } */
+
+    /* Generados automáticamente */
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Libro libro = (Libro) o;
+        return ISBN == libro.ISBN;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ISBN);
+    }
 
     public String getData(){
         return "El título es: " + title + ". El autor es: " + author + ". El ISBN es: + ISBN" ;
--- Colecciones/Default/src/UsoLibro.java
+++ Colecciones/Default/src/UsoLibro.java
@@ -2,13 +2,21 @@ public class UsoLibro {
 
     public static void main(String[] args) {
         Libro libro1 = new Libro("P en Java", "Juan", 25);
-        Libro libro2 = new Libro("P en Java", "Juan", 25);
+        Libro libro2 = new Libro("P en Java II", "Juan", 25);
+
 
         if (libro1.equals(libro2)) {
             System.out.println("Es el mismo libro");
+            System.out.println(libro1.hashCode());
+            System.out.println(libro2.hashCode());
+
         } else {
             System.out.println("No es el mismo libro");
+            System.out.println(libro1.hashCode());
+            System.out.println(libro2.hashCode());
         }
+
+
 
     }
 

dd9136d9250d140f02abdde397cd20d2ba44ac67
Rober Herraiz <herraiz92@gmail.com>
2021-10-18 14:14:26 +0100

    feat: added a interface Comparator for getting another type of sorting

--- Colecciones/Default/src/PruebaTreeSet.java
+++ Colecciones/Default/src/PruebaTreeSet.java
@@ -1,3 +1,4 @@
+import java.util.Comparator;
 import java.util.TreeSet;
 
 public class PruebaTreeSet {
@@ -20,6 +21,7 @@ public class PruebaTreeSet {
         Articulo primero = new Articulo(1, "Primer artículo");
         Articulo segundo = new Articulo(2, "Segundo artículo");
         Articulo tercero = new Articulo(3, "Tercer artículo");
+        Articulo cuarto = new Articulo(4, "Cuarto artículo");
 
         TreeSet<Articulo> ordenaArticulos = new TreeSet<>();
 
@@ -28,19 +30,37 @@ public class PruebaTreeSet {
         ordenaArticulos.add(primero);
         ordenaArticulos.add(segundo);
         ordenaArticulos.add(tercero);
+        ordenaArticulos.add(cuarto);
 
         for (Articulo articulo: ordenaArticulos) {
             System.out.println(articulo.getDesc());
         }
 
+        Articulo comparadorArticulos = new Articulo();
+
+        TreeSet <Articulo> ordenaArticulos2 = new TreeSet<>(comparadorArticulos);
+
+        ordenaArticulos2.add(primero);
+        ordenaArticulos2.add(segundo);
+        ordenaArticulos2.add(tercero);
+        ordenaArticulos2.add(cuarto);
+
+        System.out.println("");
+
+        for (Articulo art: ordenaArticulos2) {
+            System.out.println(art.getDesc());
+        }
+        
     }
 
 }
 
-class Articulo implements Comparable<Articulo> {
+class Articulo implements Comparable<Articulo>, Comparator<Articulo> {
 
     private int num;
     private String desc;
+
+    public Articulo(){}
 
     public Articulo(int num, String desc){
 
@@ -56,5 +76,12 @@ class Articulo implements Comparable<Articulo> {
 
     public String getDesc() {
         return desc;
+    }
+
+    @Override
+    public int compare(Articulo o1, Articulo o2) {
+        String descripcion1 = o1.getDesc();
+        String descripcion2 = o2.getDesc();
+        return descripcion1.compareTo(descripcion2); // comparación alfabética
     }
 }
